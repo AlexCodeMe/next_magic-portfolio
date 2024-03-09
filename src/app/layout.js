e2776adcf60b7714d22e198e0677d26abc38ebd6
alex moyer <chloegome@icloud.com>
2024-03-09 15:58:33 -0500

    fixed typo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
-import FirefliesBackground from "@/components/FireFliesBackground";
+import FirefliesBackground from "@/components/FirefliesBackground";
 
 const inter = Inter({ 
   subsets: ["latin"],
